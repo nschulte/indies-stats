f7583fdf37e3d4730dbab5f1165ea0f9b22da7ef
Nick Schulte <theschulte@gmail.com>
2016-12-05 22:14:59 -0600

    Fix bug when navigating insite

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
-chrome.tabs.executeScript(null, {file: "lib/jquery-3.1.1.min.js"}, function() {
-    chrome.tabs.executeScript(null, {file: "content.js"});
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+  chrome.tabs.executeScript(null, {file: "lib/jquery-3.1.1.min.js"}, function() {
+      chrome.tabs.executeScript(null, {file: "content.js"});
+  });
 });
