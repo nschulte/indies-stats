1f53ae4ada0c2c08d8ebc58b5888fc1e5263ef2e
Nick Schulte <theschulte@gmail.com>
2016-12-05 21:32:38 -0600

    Load jquery before content in background

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  chrome.tabs.executeScript(null, {file: "content.js"});
+chrome.tabs.executeScript(null, {file: "lib/jquery-3.1.1.min.js"}, function() {
+    chrome.tabs.executeScript(null, {file: "content.js"});
 });
